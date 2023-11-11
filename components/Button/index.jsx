036a58add22984d8d42b78c2fd2e0a987efc2010
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-11-10 20:42:30 -0800

    pagination button

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,8 +1,13 @@
-import styles from './index.module.scss'
-import cl from 'classnames'
+import styles from "./index.module.scss";
+import cl from "classnames";
 
-export const Button = ({ className, children, onClick, disabled }) => (
-  <div className={cl(className, styles.button)} onClick={onClick} disabled={disabled}>
-    {children}
-  </div>
-)
+export const Button = ({ className, children, onClick, disabled }) => {
+  return (
+    <button
+      className={cl(className, styles.button)}
+      disabled={disabled}
+      onClick={onClick}>
+      {children}
+    </button>
+  );
+};
--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -1,14 +1,17 @@
-import styles from './index.module.scss'
-import cl from 'classnames'
-import Link from 'next/link'
-import Image from 'next/image'
-import { urlFor } from '../../lib/client'
-import { Title } from '../Title'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import cl from "classnames";
+import { urlFor } from "../../lib/client";
+import styles from "./index.module.scss";
+import { Title } from "../Title";
 
 export const Post = ({ className, image, title, description, slug }) => (
-  <Link href={`/post/${encodeURIComponent(slug.current)}`} class={cl(className, styles.post)}>
+  <Link
+    href={`/post/${encodeURIComponent(slug.current)}`}
+    className={cl(className, styles.post)}>
     <a className={styles.link}>
-      <Title type='small' className={styles.title}>
+      <Title type="small" className={styles.title}>
         {title}
       </Title>
       <div className={styles.content}>
@@ -16,12 +19,12 @@ export const Post = ({ className, image, title, description, slug }) => (
           <Image
             src={urlFor(image).url()}
             alt={image.caption}
-            width="100px"
-            height="100px"
+            width="100"
+            height="100"
           />
         </div>
-        <div className={styles.description}>{description}</div>
+        <p className={styles.description}>{description}</p>
       </div>
     </a>
   </Link>
-)
+);
