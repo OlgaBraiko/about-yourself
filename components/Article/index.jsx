ed477eaaec4c99d488ec444bbc53c4e4c52be984
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-11-16 19:07:44 -0800

    content

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import cl from "classnames";
 import styles from "./index.module.scss";
+import Link from "next/link";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
-const Article = ({ className, children }) => {
+const Article = ({ className, children, backUrl }) => {
   return (
-    <article className={cl(className, styles.article)}>{children}</article>
+    <article className={cl(className, styles.article)}>
+      <Link href={backUrl}>
+        <a className={styles.back}>
+          <AiOutlineArrowLeft />
+        </a>
+      </Link>
+
+      <div className={styles.content}>{children}</div>
+    </article>
   );
 };
 export default Article;
